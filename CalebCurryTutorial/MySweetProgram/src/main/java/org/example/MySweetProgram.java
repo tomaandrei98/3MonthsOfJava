5df6ac9d6642c6a7a117de6332a597e52f2144c2
Toma <tomaandrei98@gmail.com>
2023-01-02 18:40:49 +0200

    Add linked list exemplifies stack (LIFO)

--- CalebCurryTutorial/MySweetProgram/src/main/java/org/example/MySweetProgram.java
+++ CalebCurryTutorial/MySweetProgram/src/main/java/org/example/MySweetProgram.java
@@ -4,16 +4,16 @@ import java.io.FileNotFoundException;
 import java.util.LinkedList;
 
 public class MySweetProgram {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args){
 
         LinkedList<String> names = new LinkedList<>();
-        names.add("Caleb");
-        names.add("Sue");
-        names.add("Sally");
+        names.push("Caleb");
+        names.push("Sue");
+        names.push("Sally");
 
-        //queue implementation -> FIFO
+        //stack implementation -> LIFO
+        System.out.println(names.pop());
         System.out.println(names.remove());
-        System.out.println(names.remove());
-        System.out.println(names.remove());
+        System.out.println(names.removeFirst());
     }
 }

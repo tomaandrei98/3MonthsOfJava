1e9dfc77d4946f7636610f453136e2734b038496
Toma <tomaandrei98@gmail.com>
2023-01-02 18:34:05 +0200

    Add linked list exemplifies queue (FIFO)

--- CalebCurryTutorial/MySweetProgram/src/main/java/org/example/MySweetProgram.java
+++ CalebCurryTutorial/MySweetProgram/src/main/java/org/example/MySweetProgram.java
@@ -1,26 +1,19 @@
 package org.example;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.LinkedList;
 
 public class MySweetProgram {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("C:\\Users\\Toma\\IDEA Projects\\CalebCurryTutorial\\MySweetProgram\\src\\main\\resources\\students.txt"));
 
-        List<String> students = new ArrayList<>();
+        LinkedList<String> names = new LinkedList<>();
+        names.add("Caleb");
+        names.add("Sue");
+        names.add("Sally");
 
-        while (scanner.hasNext()) {
-            students.add(scanner.nextLine());
-        }
-
-        System.out.println(students);
-
-        for (String student : students) {
-            System.out.println(student);
-        }
-        scanner.close();
+        //queue implementation -> FIFO
+        System.out.println(names.remove());
+        System.out.println(names.remove());
+        System.out.println(names.remove());
     }
 }

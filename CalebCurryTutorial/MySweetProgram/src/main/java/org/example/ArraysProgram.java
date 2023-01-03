bc06e437d0de652720f83780117f0e0199862333
Toma <tomaandrei98@gmail.com>
2023-01-03 11:20:45 +0200

    Working with 2D arrays

---
+++ CalebCurryTutorial/MySweetProgram/src/main/java/org/example/ArraysProgram.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ArraysProgram {
+    public static void main(String[] args) {
+        int[][] data = {
+                {4, 6, 3, 10},
+                {4, 2, 40, 1},
+                {5, 34, 1, 43}
+        };
+
+        data[1][2] = 49;
+        System.out.println(data[1][2]);
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

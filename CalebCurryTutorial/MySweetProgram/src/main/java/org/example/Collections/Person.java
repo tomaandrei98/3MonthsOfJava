6bd7906d6ccf515468e3a2081b9c322264e40b60
Toma <tomaandrei98@gmail.com>
2023-01-03 16:00:40 +0200

    ȚAdd override hashCode and equals methods

---
+++ CalebCurryTutorial/MySweetProgram/src/main/java/org/example/Collections/Person.java
@@ -0,0 +1,32 @@
+package org.example.Collections;
+
+import java.util.Objects;
+
+public class Person {
+    public String email;
+    public String ln;
+    public Position position;
+
+    public Person(String email, String ln) {
+        this.email = email;
+        this.ln = ln;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(email, person.email) && Objects.equals(ln, person.ln) && Objects.equals(position, person.position);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((ln == null) ? 0 : ln.hashCode());
+        result = prime * result + ((position == null) ? 0 : position.hashCode());
+        return result;
+    }
+}

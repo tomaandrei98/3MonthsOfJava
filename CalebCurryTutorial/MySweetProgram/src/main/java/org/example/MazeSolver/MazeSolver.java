b859cb635164cb516bb67261e3fecc4a3887ef6d
Toma <tomaandrei98@gmail.com>
2023-01-03 11:59:06 +0200

    Add maze movement (exceptions not managed)

--- CalebCurryTutorial/MySweetProgram/src/main/java/org/example/MazeSolver/MazeSolver.java
+++ CalebCurryTutorial/MySweetProgram/src/main/java/org/example/MazeSolver/MazeSolver.java
@@ -5,8 +5,8 @@ import java.util.LinkedList;
 public class MazeSolver {
     static int[][] maze = {
             {2, 0, 1, 1},
-            {1, 1, 0, 1},
-            {0, 1, 1, 1}
+            {1, 1, 1, 0},
+            {0, 0, 0, 1}
     };
 
     //0 = wall
@@ -16,9 +16,69 @@ public class MazeSolver {
     static LinkedList<Position> path = new LinkedList<>();
 
     public static void main(String[] args) {
-        Position p = new Position(3, 0);
+        Position p = new Position(0, 3);
         path.push(p);
 
-        maze[path.peek().y][path.peek().x] =0;
+
+        while (true) {
+            int y = path.peek().y;
+            int x = path.peek().x;
+            maze[y][x] = 0;
+
+            //down
+            if (maze[y + 1][x] == 2) {
+                System.out.println("Moved down!");
+                System.out.println("You won!");
+                return;
+            } else if (maze[y + 1][x] == 1) {
+                System.out.println("Moved down!");
+                path.push(new Position(y + 1, x));
+                continue;
+            }
+
+
+            //left
+            if (maze[y][x - 1] == 2) {
+                System.out.println("Moved left!");
+                System.out.println("You won!");
+                return;
+            } else if (maze[y][x - 1] == 1) {
+                System.out.println("Moved left!");
+                path.push(new Position(y, x - 1));
+                continue;
+            }
+
+
+            //up
+            if (maze[y - 1][x] == 2) {
+                System.out.println("Moved up!");
+                System.out.println("You won!");
+                return;
+            } else if (maze[y - 1][x] == 1) {
+                System.out.println("Moved up!");
+                path.push(new Position(y - 1, x));
+                continue;
+            }
+
+
+            //right
+            if (maze[y][x+ 1] == 2) {
+                System.out.println("Moved right!");
+                System.out.println("You won!");
+                return;
+            } else if (maze[y][x + 1] == 1) {
+                System.out.println("Moved right!");
+                path.push(new Position(y, x + 1));
+                continue;
+            }
+/*
+
+            path.pop();
+            if (path.size() < 0) {
+                System.out.println("No Path");
+            }
+*/
+
+        }
     }
 }

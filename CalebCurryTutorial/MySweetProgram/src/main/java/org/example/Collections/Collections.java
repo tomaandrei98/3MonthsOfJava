1b84705fa09bfb5eb5b3c7a4f01e7548f263ec75
Toma <tomaandrei98@gmail.com>
2023-01-03 16:43:44 +0200

    Generic wildcards and casting lists

--- CalebCurryTutorial/MySweetProgram/src/main/java/org/example/Collections/Collections.java
+++ CalebCurryTutorial/MySweetProgram/src/main/java/org/example/Collections/Collections.java
@@ -1,38 +1,23 @@
 package org.example.Collections;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Collections {
     public static void main(String[] args) {
-        Pair<Person, Person> pair = new Pair<>();
-        pair.setX(new Person("email@email.com", "hello"));
-        pair.setY(new Person("email@email.com", "hello"));
-
-        System.out.println(pair.getX());
-        System.out.println(pair.getY());
-
-        Pair<Integer, String> pair2 = new Pair<>();
-        pair2.setX(7);
-        pair2.setY("fav no");
-        System.out.println(pair2.getX());
-        System.out.println(pair2.getY());
-
-        Pair<Integer, String> pair3 = new Pair<>();
-        pair2.setX(10);
-        pair2.setY("subs no");
-
-        ArrayList<Pair<Integer, String>> pears = new ArrayList<>();
-        pears.add(pair2);
-        pears.add(pair3);
-        System.out.println(pears);
-
-        ArrayList<Person> people = new ArrayList<>();
         Admin a = new Admin();
-        people.add(a);
-        doSomething(a);
+        a.email = "Caleb@email.com";
+        a.ln = "curry";
+        ArrayList<Admin> admins = new ArrayList<>();
+        admins.add(a);
+        List<Person> people = (List<Person>) (List<?>) admins;
+
+        doSomething(people);
     }
 
-    static void doSomething(Person person) {
-        System.out.println(person);
+    static void doSomething(List<Person> peeps) {
+        for (Person p : peeps) {
+            System.out.println(p.email + " " + p.ln);
+        }
     }
 }

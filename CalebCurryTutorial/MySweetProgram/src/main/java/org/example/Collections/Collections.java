9402e33a550cddf2e1ba96f5d6b026743f93673c
Toma <tomaandrei98@gmail.com>
2023-01-03 16:13:31 +0200

    Add introduction to HashMap

--- CalebCurryTutorial/MySweetProgram/src/main/java/org/example/Collections/Collections.java
+++ CalebCurryTutorial/MySweetProgram/src/main/java/org/example/Collections/Collections.java
@@ -1,7 +1,12 @@
 package org.example.Collections;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Collections {
     public static void main(String[] args) {
+
+
         Person p = new Person("email@email.com", "Curry");
         Position pos1 = new Position(10, 10);
         p.position = pos1;
@@ -13,6 +18,21 @@ public class Collections {
         System.out.println(p.equals(q));
         System.out.println(p.hashCode());
         System.out.println(q.hashCode());
+
+        //unordered (hashCode is used to determine where to insert these elements) and no duplicates
+        HashSet<Person> people = new HashSet<>();
+        people.add(p);
+        people.add(q);
+        System.out.println(people);
+
+        Person newPerson = new Person("email@email.com", "Curry");
+        Position newPosition = new Position(10, 10);
+        newPerson.position = newPosition;
+        System.out.println(people.contains(newPerson));
+
+        HashMap<String, Person> peeps = new HashMap<>();
+        peeps.put("email@email.com", newPerson);
+        System.out.println(peeps.containsKey("email@email.com"));
 
     }
 }

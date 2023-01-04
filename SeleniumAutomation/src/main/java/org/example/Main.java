2f55c7f85fa210482472f4f329f565fff4ebb2cd
Toma <tomaandrei98@gmail.com>
2023-01-04 22:05:43 +0200

    Refactoring the project (apply the logic with methods outside the main)

--- SeleniumAutomation/src/main/java/org/example/Main.java
+++ SeleniumAutomation/src/main/java/org/example/Main.java
@@ -16,31 +16,49 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        //set up the environment
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
-
-        //getting the URL
-        driver.get("https://demowebshop.tricentis.com/");
+        //setting up the environment
+        ChromeDriver driver = setUp("https://demowebshop.tricentis.com/");
 
         //finding the anchor tags by tagName and mapping the WebElements into String by text and counting them
-        List<WebElement> anchorsElements = driver.findElements(By.tagName("a"));
-        List<String> anchorsText = anchorsElements.stream()
-                .map(webElement -> webElement.getText().replace("\n", ""))
-                .toList();
-        long numberOfAnchors = anchorsText.size();
+        List<String> anchorsText = getAnchorsText(getAnchorsElements(driver));
+        long numberOfAnchors = getNumberOfAnchors(anchorsText);
         System.out.println("In this page, we've found " + numberOfAnchors + " anchor tags!\n");
-
-
         System.out.println(anchorsText + "\n");
 
         //checking if the links contains "href"
-        Stream<WebElement> anchorTagsWithoutHrefAttribute =
-                anchorsElements.stream().filter(element -> element.getAttribute("href") == null);
-        long numberOfAnchorTagsWithoutHrefAttribute = anchorTagsWithoutHrefAttribute.count();
+        long numberOfAnchorTagsWithoutHrefAttribute = getNumberOfAnchorTagsWithoutHrefAttribute(getAnchorsElements(driver));
         System.out.println("There are " + numberOfAnchorTagsWithoutHrefAttribute + " broken links (doesn't contain href attribute)");
 
         //closing the chrome driver
         driver.quit();
+    }
+
+
+    private static ChromeDriver setUp(String url) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get(url);
+        return driver;
+    }
+
+    private static List<WebElement> getAnchorsElements(ChromeDriver driver) {
+        List<WebElement> anchorsElements = driver.findElements(By.tagName("a"));
+        return anchorsElements;
+    }
+    private static List<String> getAnchorsText(List<WebElement> anchorsElements) {
+        //finding the anchor tags by tagName and mapping the WebElements into String by text and counting them
+        List<String> anchorsText = anchorsElements.stream()
+                .map(webElement -> webElement.getText().replace("\n", ""))
+                .toList();
+        return anchorsText;
+    }
+
+    private static long getNumberOfAnchors(List<String> anchorsText) {
+        return anchorsText.size();
+    }
+
+    private static long getNumberOfAnchorTagsWithoutHrefAttribute(List<WebElement> anchorsElements) {
+        long result = anchorsElements.stream().filter(element -> element.getAttribute("href") == null).count();
+        return result;
     }
 }
